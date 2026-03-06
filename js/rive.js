985493dc79cb835fb53a203f4f52b2e328b705e0
timcartoonbase <tim@cartoonbase.com>
2026-03-06 13:48:21 +0000

    updated file names

--- js/rive.js
+++ js/rive.js
@@ -2,27 +2,27 @@ const riveCanvas = document.getElementById("riveCanvas");
 let r;
 
 r = new rive.Rive({
-  src: "assets/hesso.riv",
+  src: "assets/bridgestone_260306.riv",
   canvas: riveCanvas,
   layout: new rive.Layout({ fit: rive.Fit.Cover }),
   stateMachines: ["State Machine 1"],
   autoplay: true,
   autobind: false,
-  artboard: "Slide 16:9 - 1",
+  artboard: "Artboard",
   fitCanvasToArtboardWidth: true,
   fitCanvasToArtboardHeight: true,
   onLoad: () => {
     r.resizeDrawingSurfaceToCanvas();
     window.riveInputs = r.stateMachineInputs("State Machine 1");
 
-    const viewModel = r.viewModelByName("textBoxes");
+    const viewModel = r.viewModelByName("ViewModel1");
     const vmi = viewModel.instanceByName("Instance");
     r.bindViewModelInstance(vmi);
     console.log(vmi);
 
     // Strings
-    const textBox1 = vmi.string("textBox1");
-    textBox1.value = "Hello World";
+    const textBox1 = vmi.number("activeArea");
+    textBox1.value = 0; //Increment to activate areas. Set to 0 to activate home screen.
   },
 });
 
